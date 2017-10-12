621683c82565ea167915ec98e7cc350b52a64d4d
Alwyn Tan <alwyn.tan@data.gov.sg>
2017-10-12 09:31:34 +0800

    Initial commit
    * Create a quick and dirty Node.js proxy that checks if the client is a crawler
    
    * If so, and if it is asking for a page that we want indexed by the crawler,
    rewrite the URL and direct to the host specified in `ROBOTS_URL`
    
    * Otherwise, passthrough to the host specified in `BACKEND_URL`. If the url
    path does not contain a `.`, assume that it is an Angular view and fetch whatever
    is found at the host root
    
    * Assuming that the host contains an Angular app with `html5mode` enabled,
    the app will render the appropriate view by inspecting the path through the
    browser-side HTML5 history API

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const assert = require('assert')
+const http = require('http')
+
+const httpProxy = require('http-proxy')
+
+const CRAWLER_USER_AGENTS = /facebookexternalhit|Facebot|Twitterbot|Pinterest|Google.*snippet/
+
+const { ROBOTS_URL, BACKEND_URL, PORT } = process.env
+
+assert(BACKEND_URL, 'BACKEND_URL is not set')
+assert(ROBOTS_URL, 'ROBOTS_URL is not set')
+assert(PORT, 'PORT is not set')
+
+const proxy = httpProxy.createProxyServer({
+  changeOrigin: true,
+})
+
+function shouldRedirect (url) {
+  return url.startsWith('/tabs/route')
+}
+
+function rewrite (url) {
+  const routeIdURLFrom = (u) => {
+    const [routeId] = u.match(/\d+/) || []
+    return routeId ? `/routes/${routeId}` : '/routes'
+  }
+  return url === '/tabs/routes' ? '/routes' : routeIdURLFrom(url)
+}
+
+const listener = (req, res) => {
+  const { url } = req
+  const [robotAgent] = (req.headers['user-agent'] || '').match(CRAWLER_USER_AGENTS) || []
+
+  if (robotAgent && shouldRedirect(url)) {
+    req.url = rewrite(url)
+    proxy.web(req, res, { target: ROBOTS_URL })
+  } else {
+    const ignorePath = url.match(/\./) === null
+    proxy.web(req, res, { ignorePath, target: BACKEND_URL })
+  }
+}
+
+http.createServer(listener).listen(PORT)
