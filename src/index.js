4e33278254b12a72622709e3d313587b73d3526d
Alwyn Tan <alwyn.tan@data.gov.sg>
2017-11-23 16:57:16 +0800

    Enable operator-specific styling for Grab
    * If the query param `operator=grab` is specified, change behaviour as follows:
       * fetch `/grab.html` instead of ‘/‘ for tab paths
       * alter app name, url and image used for open graph content

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const axios = require('axios')
 const httpProxy = require('http-proxy')
 const svg2png = require('svg2png')
+const { parse } = require('url')
 const { makeOpenGraphForRoute, makeRouteIndex, makeRouteBanner } = require('./openGraph')
 
 const CRAWLER_USER_AGENTS = /facebookexternalhit|Facebot|Slackbot|TelegramBot|WhatsApp|Twitterbot|Pinterest|Googlebot|Google.*snippet|Google-Structured/
@@ -23,16 +24,19 @@ const isRouteBanner = url => url.startsWith('/images/banners/routes/')
 
 const listener = async (req, res) => {
   const { url } = req
+  const { query, pathname } = parse(url, true)
+
+  const isGrab = query.operator === 'grab'
   const [robotAgent] = (req.headers['user-agent'] || '').match(CRAWLER_USER_AGENTS) || []
   if (robotAgent && shouldRedirect(url)) {
     console.log(`Receiving request from ${req.headers['user-agent']}`)
     const [routeId] = url.match(/\d+/) || []
     const payload = routeId
-      ? await axios.get(`${ROBOTS_URL}/routes/${routeId}`).then(r => makeOpenGraphForRoute(r.data))
+      ? await axios.get(`${ROBOTS_URL}/routes/${routeId}`).then(r => makeOpenGraphForRoute(r.data, query))
       : await axios.get(`${ROBOTS_URL}/routes`).then(r => makeRouteIndex(r.data))
     res.end(payload)
-  } else if (isTabPath(url)) {
-    const payload = await axios.get(BACKEND_URL).then(r => r.data)
+  } else if (isTabPath(url) || pathname === '/') {
+    const payload = await axios.get(BACKEND_URL + (isGrab ? '/grab.html' : '')).then(r => r.data)
     res.end(payload.replace('<head>', '<head><base href="/" />'))
   } else if (isRouteBanner(url)) {
     const [routeId] = url.match(/\d+/) || []
@@ -43,7 +47,8 @@ const listener = async (req, res) => {
       const route = await axios.get(`${ROBOTS_URL}/routes/${routeId}?includeIndicative=true&includeTrips=false`).then(r => r.data)
       const { nextTripId } = route.indicativeTrip
       const trip = await axios.get(`${ROBOTS_URL}/trips/${nextTripId}`).then(r => r.data)
-      const payload = trip ? await makeRouteBanner(Object.assign(route, { trip })) : undefined
+      Object.assign(route, { trip })
+      const payload = trip ? await makeRouteBanner(route, query) : undefined
       if (!payload) {
         res.writeHead(400, { 'Content-Type': 'text/plain' })
         res.end(`${url} refers to a route with no trip`)
--- src/openGraph.js
+++ src/openGraph.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const Handlebars = require('handlebars')
 const path = require('path')
 const _ = require('lodash')
+const querystring = require('querystring')
 
 const htmlFrom = async (payload, templateFileName) => {
   const templateText = fs.readFileSync(
@@ -29,16 +30,27 @@ const makeRouteIndex = async (data) => {
   return htmlFrom(payload, 'og-route-index.html')
 }
 
-const makeOpenGraphForRoute = async (route) => {
+const makeOpenGraphForRoute = async (route, operatorQuery) => {
   const isCrowdstart = route.tags.includes('crowdstart')
   const urlPath = isCrowdstart ? `crowdstart/${route.id}/detail` : `route/${route.id}`
   const verb = isCrowdstart ? 'Crowdstart' : 'Book'
-  return htmlFrom(_.assign({ urlPath, verb }, route), 'og-route.html')
+  const app = operatorQuery.operator === 'grab' ? 'GrabShuttle' : 'Beeline'
+  const smallHero = operatorQuery.operator === 'grab'
+    ? 'https://app.beeline.sg/grab/mstile-310x310.png'
+    : 'https://www.beeline.sg/images/fb_hero_small.png'
+  const payload = {
+    operatorQuery: operatorQuery ? `?${querystring.stringify(operatorQuery)}` : '',
+    smallHero,
+    urlPath,
+    verb,
+    app,
+  }
+  return htmlFrom(_.assign(payload, route), 'og-route.html')
 }
 
 const gmtToSGTString = dateTimeString => dateTimeString.replace('Z', '+0800')
 
-const makeRouteBanner = async (route) => {
+const makeRouteBanner = async (route, operatorQuery) => {
   const isCrowdstart = route.tags.includes('crowdstart')
   const firstStopTime = new Date(route.trip.tripStops[0].time)
   const tripDate = new Date(gmtToSGTString(route.trip.date))
@@ -51,6 +63,10 @@ const makeRouteBanner = async (route) => {
       number: route.label.substring(1),
       location: isMorning ? route.from : route.to,
     },
+    height: (operatorQuery.operator === 'grab') ? 479 : 630,
+    bannerPath: (operatorQuery.operator === 'grab')
+      ? 'https://www.beeline.sg/images/fb_gs_hero_large.jpg'
+      : 'https://www.beeline.sg/images/fb_hero_large.png',
   }
   return svgFrom(payload, 'route-banner.svg')
 }
