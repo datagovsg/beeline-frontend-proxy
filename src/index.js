6d870922b9028b63d8709ff07b14b6ade31c5de4
Alwyn Tan <alwyn.tan@data.gov.sg>
2017-11-24 16:58:28 +0800

    Inject marketing and SEO copy into indexable tabs

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const proxy = httpProxy.createProxyServer({
   changeOrigin: true,
 })
 
-const shouldRedirect = url => url.startsWith('/tabs/route') || url.startsWith('/tabs/crowdstart')
+const isIndexableTab = url => url.startsWith('/tabs/route/') || (url.startsWith('/tabs/crowdstart/') && url.endsWith('/detail'))
 const isTabPath = url => url.match(/\./) === null && url.startsWith('/tabs')
 const isRouteBanner = url => url.startsWith('/images/banners/routes/')
 
@@ -32,7 +32,7 @@ const listener = async (req, res) => {
 
   const isGrab = query.operator === 'grab'
   const [robotAgent] = (req.headers['user-agent'] || '').match(CRAWLER_USER_AGENTS) || []
-  if (robotAgent && shouldRedirect(url)) {
+  if (robotAgent && isIndexableTab(url)) {
     console.log(`Receiving request from ${req.headers['user-agent']}`)
     const [routeId] = url.match(/\d+/) || []
     if (!routeId) {
@@ -47,6 +47,40 @@ const listener = async (req, res) => {
     const payload = await axios.get(`${ROBOTS_URL}/routes`).then(r => makeRouteSitemap(r.data))
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end(payload)
+  } else if (isIndexableTab(url)) {
+    const [routeId] = url.match(/\d+/) || []
+    if (!routeId) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.end(`${url} not found`)
+    } else {
+      const payloadPromise = axios.get(BACKEND_URL + (isGrab ? '/grab.html' : '')).then(r => r.data)
+      const routeDataPromise = routeId
+        ? axios.get(`${ROBOTS_URL}/routes/${routeId}`).then(r => r.data)
+        : Promise.resolve(undefined)
+
+      const [payload, routeData] = await Promise.all([payloadPromise, routeDataPromise])
+
+      const h = rawStr => rawStr.replace(/[<>&]/g, i => `&#${i.charCodeAt(0)};`)
+      const response = payload
+        .replace('<ion-nav-view></ion-nav-view>', `
+  <ion-nav-view>
+    <h1>${h(routeData.label)}: ${h(routeData.from)} – ${h(routeData.to)}</h1>
+    <h2>${h(routeData.schedule)}</h2>
+    <p>Your browser is unable to display this page correctly.</p>
+    <p>Download the Beeline app for your phone and book this route today!</p>
+
+    <a href="https://play.google.com/store/apps/details?id=sg.beeline&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+      Download from Android Play
+    </a><br/>
+    <a href="https://itunes.apple.com/sg/app/beeline-sg/id1010615256?ls=1&mt=8">
+      Download from the App Store
+    </a>
+  </ion-nav-view>
+        `)
+        .replace('<head>', '<head><base href="/" />')
+
+      res.end(response)
+    }
   } else if (isTabPath(url) || pathname === '/') {
     const payload = await axios.get(BACKEND_URL + (isGrab ? '/grab.html' : '')).then(r => r.data)
     res.end(payload.replace('<head>', '<head><base href="/" />'))
