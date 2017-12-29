56a63a044272fc53c983a2e3abce8e3fbf03cb45
shen_xujing <shen_xujing@ida.gov.sg>
2017-12-29 18:12:44 +0800

    fix rendering issue with new beeline frontend , remove the static info

--- src/index.js
+++ src/index.js
@@ -60,22 +60,6 @@ const listener = async (req, res) => {
 
       const h = rawStr => rawStr.replace(/[<>&]/g, i => `&#${i.charCodeAt(0)};`)
       const response = payload
-        .replace('<ion-nav-view></ion-nav-view>', `
-  <ion-nav-view>
-    <div style="opacity: 0.01">
-      <h1>${h(routeData.label)}: ${h(routeData.from)} – ${h(routeData.to)}</h1>
-      <h2>${h(routeData.schedule)}</h2>
-      <p>Download the Beeline app for your phone and book this route today!</p>
-
-      <a href="https://play.google.com/store/apps/details?id=sg.beeline&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-        Download from Android Play
-      </a><br/>
-      <a href="https://itunes.apple.com/sg/app/beeline-sg/id1010615256?ls=1&mt=8">
-        Download from the App Store
-      </a>
-    </div>
-  </ion-nav-view>
-        `)
         .replace('<head>', `
           <head><base href="/" />
           <meta name="keywords" content="Beeline, Beeline Singapore , GrabShuttle,  book, routes, crowdstart,  crowdsourced,  shuttle,  bus service " />
